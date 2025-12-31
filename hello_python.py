7f8373bcfa3f8b0c3896b8138a7036d360d1a9a4
Aaron C Wacker <30595158+AaronCWacker@users.noreply.github.com>
2025-12-31 09:33:15 -0600

    Enable numpy usage in hello_python.py

--- hello_python.py
+++ hello_python.py
@@ -1,8 +1,9 @@
 import js  # For DOM access
 
+
 print("Hello from Python!")
 js.document.getElementById('output').textContent = "Python executed successfully."
 
 # Example with library (uncomment after loading in runner)
-# import numpy as np
-# print(np.array([1, 2, 3]))
+import numpy as np
+print(np.array([1, 2, 3]))
